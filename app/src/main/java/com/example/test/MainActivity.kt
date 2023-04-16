fd42373596cec22bc7ffb7d14def6bf6d7f849cd
Yana Ushakova <junk7day@gmail.com>
2023-04-16 18:01:51 +0300

    test 2 - println

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println(R.string.test1)
+        println(R.string.test2)
     }
 }
